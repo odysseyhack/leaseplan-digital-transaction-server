1145d78a013b6b80cce4e25737a6b9a19c3df794
violeta.sirakova <violeta.sirakova@leaseplan.com>
2019-04-13 14:35:01 +0200

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express')
+const { asyncPoll } = require('async-poll')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const { forwardTransaction, pollBalance } = require('./transactions')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var description = 'Transaction checker \r\n POST /forward-transaction \r\n Send a raw transaction to this url and it will be forwarded to the network';
+app.get('/', (req, res) => res.send(description))
+app.post('/forward-transaction', function(req, res) {
+  forwardTransaction(req.body.Body, res)
+});
+
+const conditionFn = d => false; //keep polling
+/** Poll every 5 seconds */
+const interval = 5e3;
+
+const timeout = 30e9; //find a way to poll forever
+ 
+asyncPoll(pollBalance, conditionFn, { interval, timeout })
+  .then(console.log)
+  .catch(console.error);
+
+app.listen(port, () => console.log(`Transaction checker app listening on port ${port}!`))
+
+
---
+++ transactions.js
@@ -0,0 +1,61 @@
+var Web3 = require("web3");
+var twilio = require('twilio');
+
+var web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      process.env.ETHEREUM_TEST_NETWORK_URL
+    )
+  );
+
+var twilioClient = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+var currentBalance = -1;
+
+
+const forwardTransaction = (tx, res) => {
+  var response = '';
+
+  web3.eth
+  .sendSignedTransaction(req.body.Body, function(err, resp) {
+    if (err) {
+      console.log('error', err)
+      response = 'Something went wrong while sending your transaction. Try again.'
+    }
+    else {
+      console.log("signed transaction sent", resp)
+      response = 'Transaction sent! Your $$$ is moving!';
+    }
+
+    var twiml = new twilio.twiml.MessagingResponse();
+    twiml.message(response);
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  })
+}
+
+
+const pollBalance = async () => web3.eth
+.getBalance("0x6b98b6D48B746F8a311249d949C7dc1A6Db51A77") //violeta's rinkeby address
+.then(balance => {
+    console.log("polling")
+    if (balance != currentBalance) {
+        
+        if (currentBalance >= 0) {
+          // there's a change in balance, we need to notify the twilio app
+          console.log("balance has changed")
+          var message = `Your new balance is ${currentBalance}. ${balance > currentBalance ? 'Your funds are growing. Good on you.' : '$$$ flying away. Watch your pocket!'}`
+          console.log(message);
+          twilioClient.messages.create({
+              body: message,
+              to: '+31614610317',  
+              from: '+32460207921' 
+          })
+          .then((message) => console.log(message.sid));
+        }
+
+        currentBalance = balance;
+    }
+})
+
+module.exports.forwardTransaction = forwardTransaction;
+module.exports.pollBalance = pollBalance;
