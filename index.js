0d351ddb803c88e2a382733e6e11ff50940ebd67
violeta.sirakova <violeta.sirakova@leaseplan.com>
2019-04-13 17:52:27 +0200

    x

--- index.js
+++ index.js
@@ -37,10 +37,31 @@ const dummymethod3 = (app) => {
   app.get('/onemoreroute1', (req, res) => res.send('another one'))
 }
 
+const dummymethod4 = (app) => {
+  app.get('/dummyroute1', (req, res) => res.send('dummy route'))
+
+  app.get('/onemoreroute1', (req, res) => res.send('another one'))
+}
+
+const dummymethod5 = (app) => {
+  app.get('/dummyroute1', (req, res) => res.send('dummy route'))
+
+  app.get('/onemoreroute1', (req, res) => res.send('another one'))
+}
+
+const dummymethod6 = (app) => {
+  app.get('/dummyroute1', (req, res) => res.send('dummy route'))
+
+  app.get('/onemoreroute1', (req, res) => res.send('another one'))
+}
+
 dummymethod(app)
 dummymethod1(app)
 dummymethod2(app)
 dummymethod3(app)
+dummymethod4(app)
+dummymethod5(app)
+dummymethod6(app)
 
 app.listen(port, () => console.log(`Transaction checker app listening on port ${port}!`))
 
