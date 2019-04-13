f80e9518050df8bc834d0475fd03203049786e22
violeta.sirakova <violeta.sirakova@leaseplan.com>
2019-04-13 17:45:09 +0200

    x

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 addRoutes(app, forwardTransaction)
 startPolling(pollBalance);
 
+const dummymethod = (app) => {
+  app.get('/dummyroute1', (req, res) => res.send('dummy route'))
+
+  app.get('/onemoreroute1', (req, res) => res.send('another one'))
+}
+
+const dummymethod1 = (app) => {
+  app.get('/dummyroute1', (req, res) => res.send('dummy route'))
+
+  app.get('/onemoreroute1', (req, res) => res.send('another one'))
+}
+
+dummymethod(app)
+dummymethod1(app)
+
 app.listen(port, () => console.log(`Transaction checker app listening on port ${port}!`))
 
 
