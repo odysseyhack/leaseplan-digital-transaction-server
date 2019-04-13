440d91555226ea6f70213d5bc7fa77392411c845
violeta.sirakova <violeta.sirakova@leaseplan.com>
2019-04-13 16:57:50 +0200

    better code

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const { forwardTransaction, pollBalance } = require('./transactions/transactions
 const app = express()
 const port = process.env.PORT || 5000
 
+var description = 'Transaction checker \r\n POST /forward-transaction \r\n Send a raw transaction to this url and it will be forwarded to the network';
+app.get('/', (req, res) => res.send(description))
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 addRoutes(app, forwardTransaction);
--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const { asyncPoll } = require('async-poll')
 
 const addRoutes = (app, forwardTransaction) => {
-    var description = 'Transaction checker \r\n POST /forward-transaction \r\n Send a raw transaction to this url and it will be forwarded to the network';
-    app.get('/', (req, res) => res.send(description))
     app.post('/forward-transaction', function(req, res) {
-        console.log("forward")
         forwardTransaction(req.body.Body, res)
     });
 }
@@ -12,11 +9,11 @@ const addRoutes = (app, forwardTransaction) => {
 const startPolling = (pollBalance) => {
     const conditionFn = d => false; //keep polling
     /** Poll every 5 seconds */
-    const interval = 5e3;
+    //const interval = 5e3;
 
-    const timeout = 30e9; //find a way to poll forever
+    //const timeout = 30e9; //find a way to poll forever
 
-    asyncPoll(pollBalance, conditionFn, { interval, timeout })
+    asyncPoll(pollBalance, conditionFn, { interval: 5e3, timeout: 30e9 })
         .then(console.log)
         .catch(console.error);
 }
